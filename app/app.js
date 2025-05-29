302c4ad2c5b887434f9e5918a6c025f009932902
sanjaythangaraj <sanjaythangaraj@outlook.in>
2025-05-29 18:15:52 +0530

    tyrian basic

--- app/app.js
+++ app/app.js
@@ -1,3 +1,3 @@
 import { RockTheJvmApp } from './target/scala-3.3.5/app-fastopt'
 
-RockTheJvmApp().doSomething("app")
+RockTheJvmApp().launch("app")
--- app/src/main/scala/com/example/jobsboard/App.scala
+++ app/src/main/scala/com/example/jobsboard/App.scala
@@ -1,12 +1,42 @@
 package com.example.jobsboard
 
-import org.scalajs.dom.document
+import cats.effect.IO
+import org.scalajs.dom.{console, document}
+import tyrian.{Cmd, Html, Sub, TyrianApp}
+import tyrian.Html.*
+import tyrian.cmds.Logger
 
 import scala.scalajs.js.annotation.*
+import scala.concurrent.duration.*
+
+object App {
+  sealed trait Msg
+  case class Increment(amount: Int) extends Msg
+
+  case class Model(count: Int)
+}
 
 @JSExportTopLevel("RockTheJvmApp")
-class App {
-  @JSExport
-  def doSomething(containerId: String): Unit =
-    document.getElementById(containerId).innerHTML = "Hello from ScalaJs"
+class App extends TyrianApp[App.Msg, App.Model] {
+  import App.*
+
+  override def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) =
+    (Model(0), Cmd.None)
+
+  override def subscriptions(model: Model): Sub[IO, Msg] =
+//    Sub.None
+    Sub.every[IO](1.second).map(_ => Increment(1))
+
+  override def update(model: Model): Msg => (Model, Cmd[IO, Msg]) =
+    case Increment(amount) =>
+      // console.log("changing count by " + amount)
+      // (model.copy(count = model.count + amount), Logger.consoleLog[IO]("changing count by " + amount))
+      (model.copy(count = model.count + amount), Cmd.None)
+
+  override def view(model: Model): Html[Msg] =
+    div(
+      button(onClick(Increment(1)))("increase"),
+      button(onClick(Increment(-1)))("decrease"),
+      div(s"Tyrian running: ${model.count}")
+    )
 }
